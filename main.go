86466e35ee78e1c52bb40656bdffa05859c97846
Derek Dahl <dahlsystems@gmail.com>
2022-02-14 01:29:34 -0700

    Add Foo data structure to encapulate name and id

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Foo struct {
+	Name string `json:"name"`
+	Id   string `json:"id"`
+}
+
+type Foos []Foo
+
 func singleRecord(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Single Record Endpoint Hit")
+	foos := Foos{
+		Foo{Name: "Test Guy Name", Id: "1234"},
+	}
+
+	fmt.Println("Single Record Endpoint Hit")
+	json.NewEncoder(w).Encode(foos)
 }
 
 func handleRequests() {
