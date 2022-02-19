87fcda2e23de03c43e01c198b02308d7ae0d2750
Derek Dahl <dahlsystems@gmail.com>
2022-02-18 21:32:48 -0700

    Update Foo record

--- main.go
+++ main.go
@@ -42,6 +42,24 @@ func createFoo(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(foo)
 }
 
+// Update Single Foo
+func updateFoo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) // Get params
+	for index, item := range foos {
+		if item.Id == params["id"] {
+			foos = append(foos[:index], foos[index+1:]...)
+			var foo Foo
+			_ = json.NewDecoder(r.Body).Decode(&foo)
+			foo.Id = params["id"]
+			foos = append(foos, foo)
+			json.NewEncoder(w).Encode(foo)
+			return
+		}
+	}
+	w.WriteHeader(http.StatusNotFound)
+}
+
 // Get Single Foo
 func getFoo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -64,13 +82,14 @@ func getFoos(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	//Init router
+	//Init Router
 	router := mux.NewRouter().StrictSlash(true)
 
 	// Route Handlers / Endpoints
 	router.HandleFunc("/foo/{id}", getFoo).Methods("GET")
 	router.HandleFunc("/foos", getFoos).Methods("GET")
 	router.HandleFunc("/foo", createFoo).Methods("POST")
+	router.HandleFunc("/foo/{id}", updateFoo).Methods("PUT")
 	router.HandleFunc("/foo/{id}", deleteFoo).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
